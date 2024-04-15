162fe470516700f527a3ad36ca45657d86150dce
Vargha Csongor <Culisa1023@gmail.com>
2024-04-15 23:47:25 +0200

    create handler files

---
+++ backend/handlers/verifyRelationship.go
@@ -0,0 +1,12 @@
+package handlers
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
+)
+
+func VerifyRelationship(driver neo4j.DriverWithContext) gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
