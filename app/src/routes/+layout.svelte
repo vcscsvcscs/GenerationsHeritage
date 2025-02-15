63783dce6749241a66fdaa83539d93b20aa3fc34
Vargha Csongor <Culisa1023@gmail.com>
2025-02-15 17:01:54 +0100

    init new svelte 5 cloudflare project

---
+++ app/src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import '../app.css';
+	import { i18n } from '$lib/i18n';
+	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
+	let { children } = $props();
+</script>
+
+<ParaglideJS {i18n}>
+	{@render children()}
+</ParaglideJS>
