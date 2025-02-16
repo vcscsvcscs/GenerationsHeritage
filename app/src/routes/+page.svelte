04e159d40ed9055bbcee8214a5f640da80c4596a
Vargha Csongor <Culisa1023@gmail.com>
2025-02-16 20:26:11 +0100

    init svelte flow

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import {title, family_tree} from '$lib/paraglide/messages.js';
+    import { SvelteFlowProvider,Background, BackgroundVariant, SvelteFlow, Controls, MiniMap } from '@xyflow/svelte';
+    import type { Node, Edge, NodeTypes, NodeProps } from '@xyflow/svelte';
+
+    let nodes = $state.raw<Node[]>([]);
+    let edges = $state.raw<Edge[]>([]);
+</script>
+
+<svelte:head>
+	<title>{title({ page: family_tree() })}</title>
+</svelte:head>
+
+<div style="height:100vh;">
+	<SvelteFlowProvider>
+		<SvelteFlow bind:nodes bind:edges class="bg-base-100" fitView onlyRenderVisibleElements>
+			<Controls class="bg-base-300 text-base-content" />
+			<MiniMap class="bg-base-200" />
+		</SvelteFlow>
+	</SvelteFlowProvider>
+</div>
