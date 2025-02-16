5af2492ff295a829c7b9e0d147b658f2c257a1c0
Vargha Csongor <Culisa1023@gmail.com>
2025-02-16 16:55:53 +0100

    add home page title

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -2,8 +2,13 @@
 	import '../app.css';
 	import { i18n } from '$lib/i18n';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
+	import {title, family_tree} from '$lib/paraglide/messages.js';
 	let { children } = $props();
 </script>
+
+<svelte:head>
+	<title>{title({page:family_tree()})}</title>
+</svelte:head>
 
 <ParaglideJS {i18n}>
 	{@render children()}
