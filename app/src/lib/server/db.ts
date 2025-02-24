0161931cb491f1b862c418e6a4b1a9317f2136d6
Vargha Csongor <Culisa1023@gmail.com>
2025-02-24 14:31:06 +0100

    Fix type

--- app/src/lib/server/db.ts
+++ app/src/lib/server/db.ts
@@ -1,9 +1,9 @@
 import memgraph from 'neo4j-driver';
 import type { Driver } from 'neo4j-driver';
-import { MEMEGRAPH_URI, MEMGRAPH_USER, MEMGRAPH_PASSWORD } from '$env/static/private';
+import { MEMGRAPH_URI, MEMGRAPH_USER, MEMGRAPH_PASSWORD } from '$env/static/private';
 
-export const driverInstance: Driver = memgraph.driver(
-    MEMEGRAPH_URI || 'bolt://localhost:7687',
+export const DB: Driver = memgraph.driver(
+    MEMGRAPH_URI || 'bolt://localhost:7687',
     memgraph.auth.basic(
         MEMGRAPH_USER || 'memgraph',
         MEMGRAPH_PASSWORD || 'memgraph'
