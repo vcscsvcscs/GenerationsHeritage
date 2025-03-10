086e21e44777b2a0cb527c33fa4a5545aa09e99d
Vargha Csongor <Culisa1023@gmail.com>
2025-03-10 23:10:46 +0100

    add theme change + high res icon

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -2,6 +2,7 @@
     import {title, family_tree} from '$lib/paraglide/messages.js';
     import { SvelteFlowProvider,Background, BackgroundVariant, SvelteFlow, Controls, MiniMap } from '@xyflow/svelte';
     import type { Node, Edge, NodeTypes, NodeProps } from '@xyflow/svelte';
+	import ThemeButton from '$lib/theme-select.svelte';
 
     let nodes = $state.raw<Node[]>([]);
     let edges = $state.raw<Edge[]>([]);
@@ -10,6 +11,10 @@
 <svelte:head>
 	<title>{title({ page: family_tree() })}</title>
 </svelte:head>
+
+<div class="absolute top-2 right-2">
+    <ThemeButton />
+</div>
 
 <div style="height:100vh;">
 	<SvelteFlowProvider>
