004c0f9c0561e304b8141a4dacd68925780404bb
Vargha Csongor <Culisa1023@gmail.com>
2025-03-15 11:04:58 +0100

    remove theme button duplicate

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -2,7 +2,6 @@
     import {title, family_tree} from '$lib/paraglide/messages.js';
     import { SvelteFlowProvider,Background, BackgroundVariant, SvelteFlow, Controls, MiniMap } from '@xyflow/svelte';
     import type { Node, Edge, NodeTypes, NodeProps } from '@xyflow/svelte';
-	import ThemeButton from '$lib/theme-select.svelte';
 
     let nodes = $state.raw<Node[]>([]);
     let edges = $state.raw<Edge[]>([]);
@@ -12,10 +11,6 @@
 	<title>{title({ page: family_tree() })}</title>
 </svelte:head>
 
-<div class="absolute top-2 right-2">
-    <ThemeButton />
-</div>
-
 <div style="height:100vh;">
 	<SvelteFlowProvider>
 		<SvelteFlow bind:nodes bind:edges class="bg-base-100" fitView onlyRenderVisibleElements>
@@ -24,3 +19,4 @@
 		</SvelteFlow>
 	</SvelteFlowProvider>
 </div>
+
