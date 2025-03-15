ced4e1b89c7ad1500179e6361cfb558e5ad71413
Vargha Csongor <Culisa1023@gmail.com>
2025-03-15 11:04:01 +0100

    add theme button to layout

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -3,8 +3,12 @@
 	import { i18n } from '$lib/i18n';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	let { children } = $props();
+	import ThemeButton from '$lib/theme-select.svelte';
 </script>
 
 <ParaglideJS {i18n}>
 	{@render children()}
+	<div class="absolute top-2 right-2">
+		<ThemeButton />
+	</div>
 </ParaglideJS>
