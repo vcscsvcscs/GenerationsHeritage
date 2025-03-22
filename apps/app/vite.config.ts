c716973525a72e5b882e4488a3dc8adae47f62b6
Vargha Csongor <Culisa1023@gmail.com>
2025-03-22 16:55:33 +0100

    add code generation

--- apps/app/vite.config.ts
+++ apps/app/vite.config.ts
@@ -1,9 +1,18 @@
 import { paraglide } from '@inlang/paraglide-sveltekit/vite';
 import { defineConfig } from 'vitest/config';
+import { execSync } from "child_process";
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
 	plugins: [
+		{
+			name: "openapi-generate",
+			buildStart() {
+			  console.log("Generating TypeScript client from OpenAPI...");
+			  execSync("npx openapi-typescript ../../api/openapi.json --output src/lib/api/api.gen.ts", { stdio: "inherit" });
+			  console.log("OpenAPI client generated!");
+			}
+		},
 		sveltekit(),
 		paraglide({
 			project: './project.inlang',
