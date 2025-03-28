84b9cdf9f60bb24b6caa012985c6f208d6b34293
Vargha Csongor <Culisa1023@gmail.com>
2025-03-28 22:13:42 +0100

    add auth func

--- apps/db-adapter/internal/api/authorization_by_id.go
+++ apps/db-adapter/internal/api/authorization_by_id.go
@@ -1,1 +1,14 @@
 package api
+
+import (
+	"context"
+
+	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
+	"github.com/vcscsvcscs/GenerationsHeritage/apps/db-adapter/internal/memgraph"
+)
+
+func userWithIdHasAccessToGivenPerson(ctx context.Context, session neo4j.SessionWithContext, userId, personId int) bool {
+	_, err := session.ExecuteRead(ctx, memgraph.GetPersonById(ctx, userId))
+
+	return err == nil
+}
